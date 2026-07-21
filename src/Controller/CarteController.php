f84b0e2531c0de944faa2ae3bd2b4520ca09e135
florian0503 <florian.dimbert@next-u.fr>
2026-07-21 09:53:01 +0200

    Mise en place de Twig, AssetMapper et du layout de base

---
+++ src/Controller/CarteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CarteController extends AbstractController
+{
+    #[Route('/carte', name: 'app_carte')]
+    public function index(): Response
+    {
+        return $this->render('carte/index.html.twig');
+    }
+}
